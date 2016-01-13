ae32626772964e4cfd33fc4366dcd81ffe6216b8
Brian 'MSFT' Moore <bmoore-msft@outlook.com>
2016-01-13 13:29:24 -0600

    added to scc

---
+++ DSC/DSCConfigConfiguration.ps1
@@ -0,0 +1,106 @@
+Configuration Main
+{
+
+Param ( [string] $nodeName )
+
+Import-DscResource -ModuleName PSDesiredStateConfiguration
+
+Node $nodeName
+  {
+    WindowsFeature WebServerRole
+    {
+      Name = "Web-Server"
+      Ensure = "Present"
+    }
+   <# This commented section represents an example configuration that can be updated as required.
+    WindowsFeature WebServerRole
+    {
+      Name = "Web-Server"
+      Ensure = "Present"
+    }
+    WindowsFeature WebManagementConsole
+    {
+      Name = "Web-Mgmt-Console"
+      Ensure = "Present"
+    }
+    WindowsFeature WebManagementService
+    {
+      Name = "Web-Mgmt-Service"
+      Ensure = "Present"
+    }
+    WindowsFeature ASPNet45
+    {
+      Name = "Web-Asp-Net45"
+      Ensure = "Present"
+    }
+    WindowsFeature HTTPRedirection
+    {
+      Name = "Web-Http-Redirect"
+      Ensure = "Present"
+    }
+    WindowsFeature CustomLogging
+    {
+      Name = "Web-Custom-Logging"
+      Ensure = "Present"
+    }
+    WindowsFeature LogginTools
+    {
+      Name = "Web-Log-Libraries"
+      Ensure = "Present"
+    }
+    WindowsFeature RequestMonitor
+    {
+      Name = "Web-Request-Monitor"
+      Ensure = "Present"
+    }
+    WindowsFeature Tracing
+    {
+      Name = "Web-Http-Tracing"
+      Ensure = "Present"
+    }
+    WindowsFeature BasicAuthentication
+    {
+      Name = "Web-Basic-Auth"
+      Ensure = "Present"
+    }
+    WindowsFeature WindowsAuthentication
+    {
+      Name = "Web-Windows-Auth"
+      Ensure = "Present"
+    }
+    WindowsFeature ApplicationInitialization
+    {
+      Name = "Web-AppInit"
+      Ensure = "Present"
+    }
+    Script DownloadWebDeploy
+    {
+        TestScript = {
+            Test-Path "C:\WindowsAzure\WebDeploy_amd64_en-US.msi"
+        }
+        SetScript ={
+            $source = "http://download.microsoft.com/download/A/5/0/A502BE57-7848-42B8-97D5-DEB2069E2B05/WebDeploy_amd64_en-US.msi"
+            $dest = "C:\WindowsAzure\WebDeploy_amd64_en-US.msi"
+            Invoke-WebRequest $source -OutFile $dest
+        }
+        GetScript = {@{Result = "DownloadWebDeploy"}}
+        DependsOn = "[WindowsFeature]WebServerRole"
+    }
+    Package InstallWebDeploy
+    {
+        Ensure = "Present"  
+        Path  = "C:\WindowsAzure\WebDeploy_amd64_en-US.msi"
+        Name = "Microsoft Web Deploy 3.6 Beta"
+        ProductId = "{50638DB8-30CE-4713-8EA0-6AA405740391}"
+        Arguments = "ADDLOCAL=ALL"
+        DependsOn = "[Script]DownloadWebDeploy"
+    }
+    Service StartWebDeploy
+    {                    
+        Name = "WMSVC"
+        StartupType = "Automatic"
+        State = "Running"
+        DependsOn = "[Package]InstallWebDeploy"
+    } #>
+  }
+}
