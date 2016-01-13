c460a6b457c939059755d2824e9dbbaadcb307e5
Brian 'MSFT' Moore <bmoore-msft@outlook.com>
2016-01-13 13:20:31 -0600

    added next var

--- scripts/Write-EnvVar.ps1
+++ scripts/Write-EnvVar.ps1
@@ -1,3 +1,4 @@
 ﻿write-output "Script is executing..."
+write-output "env:artifactsLocation = $env:artifactsLocation"
 write-output "env:artifactsLocationSasToken = $env:artifactsLocationSasToken"
 write-output "Script done."
