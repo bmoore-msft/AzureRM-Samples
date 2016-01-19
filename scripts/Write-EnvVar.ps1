324d92e7c25c2c0236448e127daabcae788b280f
Brian 'MSFT' Moore <bmoore-msft@outlook.com>
2016-01-19 10:59:28 -0600

    Clean up

--- scripts/Write-EnvVar.ps1
+++
@@ -1,4 +0,0 @@
-﻿write-output "Script is executing..."
-write-output "env:artifactsLocation = $env:artifactsLocation"
-write-output "env:artifactsLocationSasToken = $env:artifactsLocationSasToken"
-write-output "Script done."
