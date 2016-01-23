b3209287112a1a4b8e4ebbdf01211f34d90cd01b
Brian 'MSFT' Moore <bmoore-msft@outlook.com>
2016-01-23 17:21:14 -0600

    updated

--- DSCTestRawFiles/dump.ps1
+++ DSCTestRawFiles/dump.ps1
@@ -1,3 +1,8 @@
-﻿Write-Output "dumping vars..."
-Write-Output $(myUri)
-Write-Output $(myToken)
+﻿param(
+    [string]$uri,
+    [string]$token
+    )
+
+Write-Output "dumping vars..."
+Write-Output "URI - $uri"
+Write-Output "Token - $token"
